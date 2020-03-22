af49893ae2552964fac325f2fe08da7a07aca92f
Vinita Gupta <vinita.gupta@publicissapient.com>
2020-03-22 21:12:26 +0530

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //const RequestHandler = require("./requestHandler.js");
 
 import './../scss/mystyles.scss';
-import './../css/mystyles.css';
+// import './../css/mystyles.css';
 require('./eventHandlers/eventListener.js');
 
 
